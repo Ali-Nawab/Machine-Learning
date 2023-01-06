f25cc70af67877e40bd9f01ae1304b7dc1a51a4b
Ali Nawab Khan15 <109417455+Ali-Nawab@users.noreply.github.com>
2023-01-06 22:45:18 +0500

    Create file1.py

---
+++ LinearRegression/file1.py
@@ -0,0 +1,164 @@
+import copy
+import math
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def compute_model_output(x, w, b):
+    """
+    Computes the prediction of a linear model
+    Args:
+      x (ndarray (m,)): Data, m examples
+      w,b (scalar)    : model parameters
+    Returns
+      y (ndarray (m,)): target values
+    """
+    m = x.shape[0]
+    f_wb = np.zeros(m)
+    for i in range(m):
+        f_wb[i] = w * x[i] + b
+
+    return f_wb
+
+
+def compute_cost(x, y, w, b):
+    """
+    Computes the cost function for linear regression.
+
+    Args:
+      x (ndarray (m,)): Data, m examples
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters
+
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0]
+
+    cost_sum = 0
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        cost_sum = cost_sum + cost
+    total_cost = (1 / (2 * m)) * cost_sum
+
+    return total_cost
+
+
+def compute_gradient(x, y, w, b):
+    """
+    Computes the gradient for linear regression
+    Args:
+      x (ndarray (m,)): Data, m examples
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b
+     """
+
+    # Number of training examples
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = f_wb - y[i]
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function):
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking
+    num_iters gradient steps with learning rate alpha
+
+    Args:
+      x (ndarray (m,))  : Data, m examples
+      y (ndarray (m,))  : target values
+      w_in,b_in (scalar): initial values of model parameters
+      alpha (float):     Learning rate
+      num_iters (int):   number of iterations to run gradient descent
+      cost_function:     function to call to produce cost
+      gradient_function: function to call to produce gradient
+
+    Returns:
+      w (scalar): Updated value of parameter after running gradient descent
+      b (scalar): Updated value of parameter after running gradient descent
+      J_history (List): History of cost values
+      p_history (list): History of parameters [w,b]
+      """
+
+    w = copy.deepcopy(w_in)  # avoid modifying global w_in
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = compute_gradient(x, y, w, b)
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db
+        w = w - alpha * dj_dw
+
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            J_history.append(cost_function(x, y, w, b))
+            p_history.append([w, b])
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
+                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                  f"w: {w: 0.3e}, b:{b: 0.5e}")
+
+    return w, b, J_history, p_history  # return w and J,w history for graphing
+
+
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300.0, 500.0])
+print(f"x_train = {x_train}")
+print(f"y_train = {y_train}")
+
+m = x_train.shape[0]
+print(f"Number of training examples is: {m}")
+m = len(x_train)
+print(f"Number of training examples is: {m}")
+i = 0
+x_i = x_train[i]
+y_i = y_train[i]
+print(f"(x^({i}), y^({i})) = ({x_i}, {y_i})")
+w = 100
+b = 100
+print(f"w: {w}")
+print(f"b: {b}")
+iterations = 20000
+tmp_alpha = 1.0e-2
+print(compute_cost(x_train, y_train, w, b))
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train, y_train, w, b, tmp_alpha,
+                                                    iterations, compute_cost, compute_gradient)
+print(f"(w,b) found by gradient descent: ({w_final:8.4f},{b_final:8.4f})")
+
+# Plot the data points
+tmp_f_wb = compute_model_output(x_train, w, b)
+plt.scatter(x_train, y_train, marker='x', c='r', label='Actual Prediction')
+plt.plot(x_train, tmp_f_wb, c='b', label='Our Prediction')
+# Set the title
+plt.title("Housing Prices")
+# Set the y-axis label
+plt.ylabel('Price (in 1000s of dollars)')
+# Set the x-axis label
+plt.xlabel('Size (1000 sqft)')
+plt.legend()
+plt.show()
